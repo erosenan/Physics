b658c9491dbb8e9e559e427b303a241fd5e56af1
1 <1@192.168.1.21>
2017-04-28 18:05:06 +0300

    direction and magnitude

--- Physics/src/gali/PhysicalObject.java
+++ Physics/src/gali/PhysicalObject.java
@@ -6,7 +6,11 @@ public class PhysicalObject {
 	private float y = 0;
 	private float vx = 0;
 	private float vy = 0;
-	public PhysicalObject(float m) {
+	private float ax = 0;
+	private float ay = 0;
+	private float m;
+	public PhysicalObject(float m_) {
+		m = m_;
 	}
 
 	public void setVelocity(float vx_, float vy_) {
@@ -16,8 +20,9 @@ public class PhysicalObject {
 
 	public void timePassed(float dt) {
 		x = x + vx * dt;
-		y = y + vy * dt; 
-		
+		y = y + vy * dt;
+		vx = vx + ( dt * ax ) / m;
+		vy = vy + ( dt * ay ) / m;
 	}
 
 	public float getX() {
@@ -28,8 +33,25 @@ public class PhysicalObject {
 //	}
 
 	public float getY() {
-		// TODO Auto-generated method stub
 		return y;
 	}
+
+	public void applyForce(float fx, float fy) {
+		ax = fx;
+		ay  = fy;
+	}
+
+	public float getVX() {
+		return vx;
+	}
+	
+	public float getVY() {
+		return vy;
+	}
+
+	public void applyForceInDirection(float f, double diraction) {
+		applyForce(f * (float)Math.cos(diraction), f * (float)Math.sin(diraction));
+	}
+	
 	
 }
--- Physics/src/gali/PhysicalObjectTest.java
+++ Physics/src/gali/PhysicalObjectTest.java
@@ -9,7 +9,7 @@ import junit.framework.Assert;
 public class PhysicalObjectTest {
 
 	@Test
-	public void test() {
+	public void testVelocity() {
 		PhysicalObject obj = new PhysicalObject(3);
 		obj.setVelocity(1.0f,-1.0f);
 		obj.timePassed(2.0f);
@@ -19,6 +19,33 @@ public class PhysicalObjectTest {
 		obj.timePassed(1.0f);
 		assertEquals(2.0f, obj.getX(),0.0001);
 		assertEquals(-2.0f, obj.getY(),0.0001);
+	}
+	
+	@Test
+	public void testAcceleration() {
+		PhysicalObject obj = new PhysicalObject(1f);
+		obj.applyForce(1f, -1f);
+		obj.timePassed(2f);
+		assertEquals(2f, obj.getVX(), 0.0001);
+		assertEquals(-2f, obj.getVY(), 0.0001);
+	}
+
+	@Test
+	public void testForce() {
+		PhysicalObject obj = new PhysicalObject(2f);
+		obj.applyForce(1f, -1f);
+		obj.timePassed(2f);
+		assertEquals(1f, obj.getVX(), 0.0001);
+		assertEquals(-1f, obj.getVY(), 0.0001);
+	}
+	
+	@Test
+	public void testDirectonalForce() {
+		PhysicalObject obj = new PhysicalObject(2f);
+		obj.applyForceInDirection(2f, Math.atan2(3f, -2f));
+		obj.timePassed(1f);
+		assertEquals(1f*1f, obj.getVX() * obj.getVX() + obj.getVY() * obj.getVY(), 0.0001);
+		assertEquals(3f/-2f, obj.getVY()/obj.getVX(), 0.0001);
 	}
 
 }
